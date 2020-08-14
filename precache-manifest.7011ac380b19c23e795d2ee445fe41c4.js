029d53dc565bd0e336686f6f5b4ef03c0ada4516
Aydriy <smitas4511@gmail.com>
2020-08-14 10:45:15 +0300

    Updates

---
+++ precache-manifest.7011ac380b19c23e795d2ee445fe41c4.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0564d3518cf74ecaeb8c3d35fc066dc",
+    "url": "/geoportal/index.html"
+  },
+  {
+    "revision": "64f73871bf574c167cb1",
+    "url": "/geoportal/static/css/main.a3f94787.chunk.css"
+  },
+  {
+    "revision": "986ca74fa953bc28e93a",
+    "url": "/geoportal/static/js/2.e89f7766.chunk.js"
+  },
+  {
+    "revision": "0c293bd607f109ed8a6b760913f9f31f",
+    "url": "/geoportal/static/js/2.e89f7766.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64f73871bf574c167cb1",
+    "url": "/geoportal/static/js/main.6c268db3.chunk.js"
+  },
+  {
+    "revision": "7c239a53ce3ece1da57d",
+    "url": "/geoportal/static/js/runtime-main.eb192b75.js"
+  },
+  {
+    "revision": "85d339d916479f729938d2911b85bf1f",
+    "url": "/geoportal/static/media/Lato-Bold.85d339d9.ttf"
+  },
+  {
+    "revision": "2d36b1a925432bae7f3c53a340868c6e",
+    "url": "/geoportal/static/media/Lato-Regular.2d36b1a9.ttf"
+  },
+  {
+    "revision": "1025a6e0fb0fa86f17f57cc82a6b9756",
+    "url": "/geoportal/static/media/OpenSans-Bold.1025a6e0.ttf"
+  },
+  {
+    "revision": "fb7e3a294cb07a54605a8bb27f0cd528",
+    "url": "/geoportal/static/media/OpenSans-ExtraBold.fb7e3a29.ttf"
+  },
+  {
+    "revision": "0eb354db988944eeef3ef0cb470bb7a6",
+    "url": "/geoportal/static/media/building.0eb354db.jpg"
+  },
+  {
+    "revision": "262c2b0e1c455f8dd9fef81e4b967d60",
+    "url": "/geoportal/static/media/building.262c2b0e.png"
+  },
+  {
+    "revision": "31460841e48e7ce5d62d2712b29ab3db",
+    "url": "/geoportal/static/media/cards-bg.31460841.png"
+  },
+  {
+    "revision": "a0fdd40ae74bfcd8d7d3d9821d9a2194",
+    "url": "/geoportal/static/media/contact-bg.a0fdd40a.png"
+  },
+  {
+    "revision": "110a5032b4357bfefa71da39abd04690",
+    "url": "/geoportal/static/media/image-bg.110a5032.png"
+  },
+  {
+    "revision": "6adb832d50df0730bfd21d6944af056d",
+    "url": "/geoportal/static/media/tabs-bg.6adb832d.png"
+  }
+]);
